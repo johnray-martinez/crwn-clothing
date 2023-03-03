9f34083f8b41788bfa964b81d3f2e6cb70b0b1bd
johnray-martinez <jrvmartinez@icloud.com>
2023-03-03 15:45:25 -0500

    added category list style

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -3,8 +3,11 @@ import './index.styles.scss';
 const Category = ({title, imageUrl,}) => {
   return (
     <div className='category'>
-      <img src={imageUrl} alt={title}/>
-      <p>{title}</p>
+      <img className='category__bg' src={imageUrl} alt={title}/>
+      <div className='category__content-container'>
+        <h2 className='category__title'>{title}</h2>
+        <p className='category__subtitle'>Shop Now</p>
+      </div>
     </div>
   );
 }
