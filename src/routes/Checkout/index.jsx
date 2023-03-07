3d65e04b3fc626cb4771806a0ccecd734ca39daf
johnray-martinez <johnraymartinezwebdev@gmail.com>
2023-03-07 07:42:53 -0500

    added logic for total price

--- src/routes/Checkout/index.jsx
+++ src/routes/Checkout/index.jsx
@@ -6,7 +6,8 @@ const Checkout = () => {
   const { cart, addItemToCart, removeItemToCart } = useContext(CartContext);
 
   const products = [...cart.values()];
-  
+  let totalPrice = 0;
+
   return(
     <main className='checkout'>
       <div className='checkout__table'>
@@ -19,6 +20,7 @@ const Checkout = () => {
         </div>
         {products.map((product) => {
           const {id, name, imageUrl, quantity, price} = product;
+          totalPrice += (price * quantity);
 
           return <div key={id} className='checkout__table-row'>
             <div className='checkout__table-image-container'>
@@ -30,10 +32,13 @@ const Checkout = () => {
               {quantity} 
               <span className='checkout__table-control' onClick={() => addItemToCart(product)}>  {' >'} </span>
             </p>
-            <p>{price}</p>
+            <p>${price * quantity}</p>
             <p className='checkout__table-control' onClick={() => removeItemToCart(id, true)}>X</p>
           </div>
         })}
+        <div className='checkout__table-row checkout__table-row--no-border checkout__table-row--right-align'>
+          <p className='checkout__table-total'>Total: ${totalPrice}</p>
+        </div>
       </div>
     </main>
   );
