2dfa4cc81d0715a4e86fadc90c2e9b7b545bd2af
johnray-martinez <johnraymartinezwebdev@gmail.com>
2023-03-10 11:08:15 -0500

    reverted back the persist

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.styles.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals'; 
 import {BrowserRouter} from 'react-router-dom'; 
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import { Provider } from 'react-redux'
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,9 +14,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        {/* <PersistGate persistor={persistor}> */}
+        <PersistGate persistor={persistor}>
           <App />   
-        {/* </PersistGate> */}
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './root-reducer';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
   Boolean
 );
 
+const persistConfig = {
+  key: 'counter',
+  storage,
+  blacklist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares)
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  }).concat(middlewares)
 });
+
+export const persistor = persistStore(store);
