5515f4c0a96b659eb0c2107cfc1ed1839e5e6368
johnray-martinez <johnraymartinezwebdev@gmail.com>
2023-03-18 20:29:31 -0400

    added lazy loading and suspense for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { useAppDispatch } from './store/store';
 import {Routes, Route} from 'react-router-dom';
-import Navigation from './routes/Navigation';
-import Home from './routes/Home';
-import Authentication from './routes/Authentication';
-import Shop from './routes/Shop';
-import Contact from './routes/Contact';
-import Checkout from './routes/Checkout';
 import { checkSessionAsync } from './store/user/userThunks';
+import Navigation from './routes/Navigation';
 import './App.css';
+import Spinner from './components/Spinner';
+const Home = lazy(() => import('./routes/Home'));
+const Authentication = lazy(() => import('./routes/Authentication'));
+const Shop = lazy(()=> import('./routes/Shop'));
+const Contact = lazy(() => import('./routes/Contact'));
+const Checkout = lazy(() => import('./routes/Checkout'));
 
 const App = () => {
   const dispatch = useAppDispatch();
@@ -19,15 +19,17 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}> 
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='sign-in' element={<Authentication />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path='/' element={<Navigation />}> 
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='sign-in' element={<Authentication />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
